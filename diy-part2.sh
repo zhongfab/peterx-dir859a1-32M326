a9ba38eb5c8966a6c9a81e1775db141c5ffef729
zhongfab <148003017+zhongfab@users.noreply.github.com>
2024-04-05 01:07:39 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,6 +13,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-sed -i 's/IMAGE_SIZE := 15872k/IMAGE_SIZE := 32256k/g' /target/linux/ath79/image/generic.mk
-sed -i 's/0xf80000/0x1f80000/g' /target/linux/ath79/dts/qca9563_dlink_dir-859-a1.dts
-sed -i 's/0xff0000/0x1ff0000/g' /target/linux/ath79/dts/qca9563_dlink_dir-859-a1.dts
+#sed -i 's/IMAGE_SIZE := 15872k/IMAGE_SIZE := 32256k/g' /target/linux/ath79/image/generic.mk
+#sed -i 's/0xf80000/0x1f80000/g' /target/linux/ath79/dts/qca9563_dlink_dir-859-a1.dts
+#sed -i 's/0xff0000/0x1ff0000/g' /target/linux/ath79/dts/qca9563_dlink_dir-859-a1.dts
